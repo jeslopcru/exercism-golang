618228b73b397ff5301637cc00946c8396e59e77
jeslopcru <jeslopcru@gmail.com>
2018-01-14 23:24:14 +0100

    leap: removing comments

--- leap/leap.go
+++ leap/leap.go
@@ -1,11 +1,5 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package leap
 
-// IsLeapYear should have a comment documenting it.
 func IsLeapYear(aNumber int) bool {
 
 	isDivisibleByFour := aNumber%4 == 0
